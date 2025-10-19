d5fbaacabead01ddbeb9bc0de36605fda6d61fab
shinwon <younggoo7000@naver.com>
2025-10-19 11:23:15 +0900

    Remove unneccessary code

--- SpringGitWork/src/main/java/com/spring/finall/view/controller/OnedayClassController.java
+++ SpringGitWork/src/main/java/com/spring/finall/view/controller/OnedayClassController.java
@@ -14,28 +14,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.spring.finall.WorkImgVO;
 import com.spring.finall.service.OneDayClassService;
 import com.spring.finall.user.OneDayClassVO;
-import com.spring.finall.user.ReserveRestVOService;
+
 
 @Controller
 @RequestMapping("/oneday")
 public class OnedayClassController {
-	@Autowired
-	private ReserveRestVOService reserveRestServie;
+
 
 	@Autowired
 	private OneDayClassService oneDayClassService;
-
-
-
-	@GetMapping("/classDetail")
-	public String showclassDetail(@RequestParam("onedayClass_num") int classNum, Model model) {
-		// 서비스에서 해당 클래스 정보 가져오기
-
-		return "classDetail";
-	}
-
-	
-
 	
 
 	// 다른 후기 버튼 클릭시 호출된다.
