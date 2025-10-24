12bd60dcb9bdc0b5105de6a09d1f8b3e5b6f761d
shinwon <younggoo7000@naver.com>
2025-10-24 22:22:03 +0900

    Remove unnecessary Files,
    서버 오류 유발 파일 삭제 및 Lombok 제거
    
    - admin 폴더 삭제 (서버 배포 오류 방지)
    - cmd 폴더 삭제 (학습용)
    - 프로젝트 .classpath에서 Lombok 삭제

--- SpringGitWork/src/main/java/com/spring/finall/admin/adminRepository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.finall.admin.adminRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.finall.admin.admindomain.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
--- SpringGitWork/src/main/java/com/spring/finall/admin/adminRepository/UserInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.finall.admin.adminRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.finall.admin.admindomain.UserEntity;
-
-public interface UserInfoRepository extends JpaRepository<UserEntity,Long> {
-
-}
